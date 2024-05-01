e1754f2d20a8bc536f773afe9aeaf5b9cc0f209a
hadithya <hhadithya34@gmail.com>
2024-05-01 16:00:09 +0530

    test commit

--- app.vue
+++ app.vue
@@ -1,16 +1,9 @@
 <template>
   <div>
     <H1>{{name}}</H1>
-    <ul>
-      <li v-for="project in projects" :key="project.name"></li>
-    </ul>
   </div>
 </template>
 
 <script setup>
   const name = "Harindu Hadithya"
-  const projects = [
-    {name: "Project 1"},
-    {name: "Project 2"}
-  ]
 </script>
